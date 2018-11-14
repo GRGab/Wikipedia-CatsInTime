eefcd4e8e2a2f53bfadea9c3cba8c2ad1e7805a1
Gabriel Goren Roig <gabrielgoren@gmail.com>
2018-11-14 18:01:42 -0300

    Sigo con cazador.py. Muevo funciones deprecadas a funciones_varias.py.

--- cazador.py
+++ cazador.py
@@ -1,49 +1,61 @@
-import requests as req
-
-def queryurl(lang, fmt, cmlimit, lst, cmtype, cmtitle):
-    url = 'https://{}.wikipedia.org/w/api.php?action=query'.format(lang)
-    url += '&format={}'.format(fmt)
-    url += '&cmlimit={}'.format(cmlimit)
-    url += '&list={}'.format(lst)
-    url += '&cmtype={}'.format(cmtype)
-    url += '&cmtitle={}'.format(cmtitle)
-    return url
+import requests
 
 class CazadorDeDatos():
+    """
+    COSAS IMPORTANTES A IMPLEMENTAR
+    -------------------------------
+        - Implementar función que agarra toda la data de una página dada
+            - Los hipervínculos
+            - El texto
+            - Las etiquetas
+            - Revisiones de distintas versiones
+                - Determinar si podemos agarrar hipervínculos y etiquetas
+                viejas además del texto.
+        - Implementar pedir data de varias páginas simultáneamente.
+        Esto es importante! Del manual de la API: 
+            Do not ask for one page at a time – this is very inefficient,
+            and consumes lots of extra resources and bandwidth. Instead
+            you should request information about multiple pages by
+            combining their titles or ids with the '|' pipe symbol:
+            titles=PageA|PageB|PageC
+        - Implementar función que realice el "crawl", agarrando
+        hipervínculos de páginas previas y siguiéndolos. Antes de pedir
+        la data de una nueva página, chequear que no haya sido adquirida
+        antes.
+    """
     def __init__(self, language='en', fmt='json', cmlimit=500):
         self.language = language
         self.format = fmt
         self.cmlimit = cmlimit
 
-    def get_subcats(self, nombre, ids=False):
-        cmtitle = 'Category:{}'.format(nombre)
-        url = queryurl(self.language, self.format, self.cmlimit,
-                       'categorymembers', 'subcat', cmtitle)
-        el_json = req.get(url).json()
-        if ids:
-            subcats = [elem['pageid'] for elem in el_json['query']['categorymembers']]
-        else:
-            subcats = [elem['title'] for elem in el_json['query']['categorymembers']]
-        return subcats
-
-    def get_pages(self, nombre):
-        pass
+    def query(self, pedido):
+        pedido['action'] = 'query'
+        pedido['format'] = self.format
+        pedido['cmlimit'] = self.cmlimit
+        lastContinue = {}
+        query_results = {}
+        while True:
+            # Clone original request
+            pedido2 = pedido.copy()
+            # Modify it with the values returned in the 'continue' section of the last result.
+            pedido2.update(lastContinue)
+            # Call API
+            result = requests.get('https://{}.wikipedia.org/w/api.php'.format(self.language),
+                                  params=pedido2).json()
+            if 'error' in result:
+                raise Exception(result['error'])
+            if 'warnings' in result:
+                print(result['warnings'])
+            if 'query' in result:
+                query_results.update(result['query'])
+            if 'continue' not in result:
+                break
+            lastContinue = result['continue']
+        return query_results
     
 
 
 if __name__ == '__main__':
-    # pruebas a manopla
-
-    resp = req.get('https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=page&cmtitle=Category:Physics')
-    # un_json = resp.json()
-    # pages = req.get('https://en.wikipedia.org/w/api.php?action=query&format=json&cmlimit=500&list=categorymembers&cmtype=page&cmtitle=Category:Physics').json()
-    # subcats = req.get('https://en.wikipedia.org/w/api.php?action=query&format=json&cmlimit=500&list=categorymembers&cmtype=subcat&cmtitle=Category:Physics').json()
-    # subcats = subcats['query']['categorymembers']
-    # algo = req.get('https://en.wikipedia.org/w/api.php?action=query&format=json&cmlimit=500&list=categorymembers&cmtype=file&cmtitle=Category:Physics').json()
-
-    # Pruebas de la clase
-
-    una_url = queryurl('en', 'json', 500, 'categorymembers', 'subcat', 'Category:Physics')
     caza = CazadorDeDatos()
-    subcats_physics = caza.get_subcats('Physics')
-    subcats_physics_ids = caza.get_subcats('Physics', ids=True)
+    pedido = {'list': 'categorymembers', 'cmtype': 'page', 'cmtitle': 'Category:Physics'}
+    resultado = caza.query(pedido)
