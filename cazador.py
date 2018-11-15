208b4757df7e3878752cbcb519ac058414a766e2
Gabriel Goren Roig <gabrielgoren@gmail.com>
2018-11-15 12:41:03 -0300

    Corrijo bug en CazadorDeDatos.query, ahora el método es un generador. Reestructuro la creación de la llamada a la API en varios métodos separados. Agrego además métodos contar_items y get_links_pagesincat.

--- cazador.py
+++ cazador.py
@@ -23,19 +23,23 @@ class CazadorDeDatos():
         la data de una nueva página, chequear que no haya sido adquirida
         antes.
     """
-    def __init__(self, language='en', fmt='json', limit=500):
+    def __init__(self, language='en', fmt='json', limit='max'):
         self.language = language
         self.format = fmt
         self.limit = limit
+        # actiondict es el dict con parámetros que se usan siempre
+        self.actiondict = self.set_actiondict()
 
-    def query(self, pedido, continuar=True):
-        pedido = pedido.copy()
-        pedido.update({'action': 'query', 'format': self.format, 'redirects': ''})
-        
-        # Para que el formato sea el correcto
-        if pedido['format'] in ['json', 'php']:
-            pedido['formatversion'] = '2'
-        # Seteamos los límites de elementos a sus valores máximos
+    def set_actiondict(self):
+        actiondict = {'action': 'query',
+                      'format': self.format,
+                      'redirects': ''}
+        # Para que el formato sea el correcto:
+        if actiondict['format'] in ['json', 'php']:
+            actiondict['formatversion'] = '2'
+        return actiondict
+
+    def set_limits(self, pedido):
         if 'cmtitle' in pedido.keys():
             pedido['cmlimit'] = self.limit
         if 'generator' in pedido.keys():
@@ -48,11 +52,19 @@ class CazadorDeDatos():
                 pedido['pllimit'] = self.limit
             if 'categories' in pedido['prop']:
                 pedido['cllimit'] = self.limit
+        return pedido
+
+    def query(self, pedido, continuar=True):
+        pedido = pedido.copy()
+        pedido.update(self.actiondict)
+        # Seteamos los límites
+        pedido = self.set_limits(pedido)
         # Comenzamos las llamadas a la API
-        query_results = {}
         if continuar:
             lastContinue = {}
+            acc = 0
             while True:
+                acc += 1
                 # Clone original request
                 pedido2 = pedido.copy()
                 # Modify it with the values returned in the 'continue' section of the last result.
@@ -61,30 +73,77 @@ class CazadorDeDatos():
                 result = requests.get('https://{}.wikipedia.org/w/api.php'.format(self.language),
                                     params=pedido2).json()
                 if 'batchcomplete' in result and result['batchcomplete']==True:
-                    # hacer algo
-                    pass
+                    print('Batch completo!')
                 if 'error' in result:
-                    raise Exception(result['error'])
+                    # Mepa que no queremos que se eleve un error porque eso
+                    # interrumpe la ejecución
+                    # raise Exception(result['error'])
+                    print('ERROR:', result['error'])
                 if 'warnings' in result:
                     print(result['warnings'])
                 if 'query' in result:
-                    query_results.update(result['query'])
+                    r = result['query']
+                    if 'pages' in r.keys():
+                        print('# de páginas adquiridas: ', len(r['pages']))
+                        print('# de links adquiridos:', contar_items(r)[1])
+                    yield r
                 if 'continue' not in result:
                     break
                 lastContinue = result['continue']
+            print('# de llamadas a la API:', acc)
         else:
             result = requests.get('https://{}.wikipedia.org/w/api.php'.format(self.language),
                                     params=pedido).json()
             if 'batchcomplete' in result and result['batchcomplete']==True:
-                # hacer algo
-                pass
+                print('Batch completo!')
             if 'error' in result:
-                raise Exception(result['error'])
+                # raise Exception(result['error'])
+                print('ERROR:', result['error'])
             if 'warnings' in result:
                 print(result['warnings'])
             if 'query' in result:
-                query_results = result['query']
-        return query_results
+                return result['query']
+    
+    def get_links_pagesincat(self, category_name):
+        """Obtiene todos los links correspondientes a todas las
+        páginas que pertenecen a la categoría `category_name`.
+        No obtiene nada relacionado con las subcategorías.
+        """
+        pedido = {'generator': 'categorymembers',
+                  'gcmtitle': category_name,
+                  'gcmtype': 'page',
+                  'prop': 'links'}
+        data = {}
+        for result in self.query(pedido):
+            pages = result['pages']
+            n_pages_temp = len(pages)
+            for i in range(n_pages_temp):
+                title = pages[i]['title']
+                if title not in data.keys():
+                    # Si nunca pasamos por esta página, le creo
+                    # un dict para que guarde allí los links luego
+                    data[title] = {'links': []}
+                if 'links' in pages[i].keys():
+                    linknames = [l['title'] for l in pages[i]['links']]
+                    # Si hay links para agregar, los agrego
+                    data[title]['links'] += linknames
+        print('# de páginas visitadas:', len(data.keys()))
+        n_links = sum(len(data[title]['links']) for title in data.keys())
+        print('# de links obtenidos en total:', n_links)
+        return data
+                
+
+def contar_items(query_result):
+    """
+    Función auxiliar que te tira cuántas páginas y cuántos links hay
+    en un dado result
+    """
+    pages = query_result['pages']
+    n_pages = len(pages)
+    get_links = lambda dic: dic['links'] if 'links' in dic.keys() else []
+    n_links_perpage = [len(get_links(pages[i])) for i in range(n_pages)]
+    n_links_tot = sum(n_links_perpage)
+    return n_pages, n_links_tot
 
 if __name__ == '__main__':
     caza = CazadorDeDatos()
@@ -97,17 +156,16 @@ if __name__ == '__main__':
 
     # res4 = caza.query({'titles': 'Physics', 'prop': 'links', 'generator': 'links'}, continuar=False)
 
-    res5 = caza.query({'gcmtitle': 'Category:Physics',
-                       'prop': 'links',
-                       'generator': 'categorymembers',
-                       'gcmtype': 'page'
-                       }, continuar=False)
+    # res5 = caza.query({'gcmtitle': 'Category:Physics',
+    #                    'prop': 'links',
+    #                    'generator': 'categorymembers',
+    #                    'gcmtype': 'page'
+    #                    }, continuar=True)
     
-    res6 = caza.query({'gcmtitle': 'Category:Physics',
-                       'generator': 'categorymembers',
-                       'gcmtype': 'subcat'
-                       }, continuar=False)
-
+    # res6 = caza.query({'gcmtitle': 'Category:Physics',
+    #                    'generator': 'categorymembers',
+    #                    'gcmtype': 'subcat'
+    #                    }, continuar=False)
 
     # pedido = {'action':'query',
     #           'titles': 'Category:Physics',              
@@ -117,3 +175,5 @@ if __name__ == '__main__':
     #           'redirects': '',
     #           }
     # resu = caza.query(pedido)
+
+    data = caza.get_links_pagesincat('Category:Physics')
