724bca32647349ed0580fa6766e0b18c4632807a
Gabriel Goren Roig <gabrielgoren@gmail.com>
2018-12-07 11:18:06 -0300

    Emprolijo informacion_mutua.py

--- informacion_mutua.py
+++ informacion_mutua.py
@@ -49,28 +49,23 @@ def calcular_ims(grafos, mutual_info='normal'):
         ims.append(im)
     return ims
 
-im_hip1 = calcular_ims(gs_hip, 'normal')
-print(im_hip1)
+# im_hip1 = calcular_ims(gs_hip, 'normal')
+# print(im_hip1)
 im_hip2 = calcular_ims(gs_hip, 'normalized')
 print(im_hip2)
-im_hip3 = calcular_ims(gs_hip, 'adjusted')
-print(im_hip3)
+# im_hip3 = calcular_ims(gs_hip, 'adjusted')
+# print(im_hip3)
 
-im_lsa1 = calcular_ims(gs_lsa, 'normal')
-print(im_lsa1)
+# im_lsa1 = calcular_ims(gs_lsa, 'normal')
+# print(im_lsa1)
 im_lsa2 = calcular_ims(gs_lsa, 'normalized')
 print(im_lsa2)
-im_lsa3 = calcular_ims(gs_lsa, 'adjusted')
-print(im_lsa3)
+# im_lsa3 = calcular_ims(gs_lsa, 'adjusted')
+# print(im_lsa3)
 
 
-im_lsa = []
-for g in gs_lsa:
-    infomap_dict = nx.get_node_attributes(g, 'infomap')
-    category_dict = nx.get_node_attributes(g, 'category')
-    infomaps, categories = [], []
-    for name in infomap_dict.keys():
-        infomaps.append(infomap_dict[name])
-        categories.append(category_dict[name])
-    im = normalized_mutual_info_score(infomaps, categories)
-    im_lsa.append(im)
+# Resultados para normalized info score
+## IM entre 'infomap' y 'category' para Hip
+## [0.4201, 0.4298, 0.4314, 0.4348]
+## IM entre 'infomap' y 'category' para LSA
+## [0.4411, 0.4547, 0.4662, 0.4829]
